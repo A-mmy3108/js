32250cd4572c50c970b39da5cf9996b634241939
A-mmy3108 <madaansingh3108@gmail.com>
2025-02-22 19:57:23 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict";  //treat all JS code as a new version of java script 
+
+// alert(3+3) we are using nodejs, not browser 
+
+
+// Code readability should be number one priority
+
+let name = "ammy"
+let age = 25
+
+let isLoggedIn = false
+let state;
+
+// number => 2 to power of 53
+// bigint = used for big numbers (not used much) 
+// string => ""
+// boolean => trues/false
+// null => standalone value
+// undefined => value not assigned
+// symbol => unique 
+
+// object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object

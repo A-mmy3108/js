3a7f1836ea284071b1852e6addcb52d7f204a10d
A-mmy3108 <madaansingh3108@gmail.com>
2025-02-22 20:37:30 +0000

    conversion

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,29 @@
+
+let score = 33
+console.log(typeof score); // typeof is used to known the datatype) 
+
+
+//CONVERSION ( CONVERTING A VALUE TO NUMBER VALUE )
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// "33" = 33
+// "33abc" = NaN (Not a Number)
+// "true" = 1
+// "false" = 0
+// "null" = 0
+// "undefined" = NaN
+
+
+
+//CONVERSION ( CONVERTING A VALUE TO BOOLEAN TYPE )
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 = true 
+// 0 = false
+// "ammy" = true
+// "" = false
